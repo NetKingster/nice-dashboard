6b149abb567c802f3ee2b9ad6b8131c8025a4c14
unknown <Administrator@SCM1505CF117.SHIMANOACE.LOCAL>
2023-04-05 16:32:20 +0800

    Update file

--- routes/routes.js
+++ routes/routes.js
@@ -7,10 +7,6 @@ const Model = require('../models/model');
 		res.render('index.ejs')
 	});
 
-	router.get('/register', (req, res)=>{
-		res.render('Register.ejs')
-	});
-
 //Post Method
 router.post('/post', async (req, res) => {
     const data = new Model({
